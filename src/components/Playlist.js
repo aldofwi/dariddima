09a29351a779ac0b9d5c2dc993a56b2168b4b6be
Aldo FWI <aldo.fwi@gmail.com>
2023-01-25 00:49:05 -0400

    Full Screen Image 25%

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,21 +4,8 @@ import playerContext from '../components/playerContext'
 
 let Playlist = () => {
 
-    const {
-        songslist, 
-        currentSong, 
-        SetCurrent} = useContext(playerContext)
-
-  return (
-
-    <div className="playlist">
-
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-
-        <ul className="loi">
-
+    /*
+    // INSIDE UL
             { songslist.map((song, i) => 
                 <li className={ 'songContainer' + (currentSong===i ? 'selected' : '')}
                     key={i}
@@ -33,6 +20,21 @@ let Playlist = () => {
                     
                 </li>
             )}
+*/
+
+    const {
+        songslist, 
+        currentSong, 
+        SetCurrent} = useContext(playerContext)
+
+  return (
+
+    <div className="playlist">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+        <ul className="loi">
 
             <div>
                 <img className="picture" src={songslist[currentSong].img} alt="" />
